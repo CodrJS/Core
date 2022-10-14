974de4477d656ba91fc1ea28f585930248703355
DylanBulmer <dylan@bulmersolutions.com>
2022-10-14 11:40:30 -0400

    update examples, adjust config test

--- src/__tests__/Config.test.ts
+++ src/__tests__/Config.test.ts
@@ -1,50 +1,16 @@
 import ProjectConfiguration, { ConfigOptions } from "../models/Config";
+import options from "../examples/project.json";
 
 describe("Project Configuration", () => {
-  const options: ConfigOptions = {
-    general: {
-      type: "code-labeling",
-      title: "My Project",
-      slug: "my-project",
-      bgColorClass: "bg-pink-600",
-      guidelines: "My guidelines go here.",
-    },
-    display: {
-      inputs: [
-        {
-          type: "text",
-          language: "Java",
-          value: "class MyClass {}",
-        },
-        {
-          type: "text",
-          language: "Java",
-          value: "class MyClass2 {}",
-        },
-      ],
-      outputs: [
-        {
-          type: "range",
-          range: [1, 10],
-        },
-        {
-          type: "short-text",
-        },
-      ],
-    },
-    model: {
-      type: "string",
-      id: "string",
-    },
-  };
-
   const options2: ConfigOptions = JSON.parse(JSON.stringify(options));
   delete options2.general.slug;
   delete options2.general.guidelines;
   delete options2.general.bgColorClass;
 
   it("does not throw an error", () => {
-    const config = ProjectConfiguration.from(options);
+    const config = ProjectConfiguration.from(
+      options as unknown as ConfigOptions,
+    );
 
     expect(config.bgColorClass).toBe("bg-pink-600");
     expect(config.title).toBe("My Project");
@@ -58,7 +24,9 @@ describe("Project Configuration", () => {
   it("generates missing options", () => {
     const config = ProjectConfiguration.from(options2);
 
-    expect(config.slug).toBe(`my-project-${new Date().toISOString().split("T")[0]}`);
+    expect(config.slug).toBe(
+      `my-project-${new Date().toISOString().split("T")[0]}`,
+    );
     expect(config.display.inputs[0]).toEqual({
       type: "text",
       language: "Java",
